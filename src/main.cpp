d86285aef5a2c6c0d07136620c38e6aeea1b9d52
LordMax2 <max.klevebrand@gmail.com>
2026-05-18 07:56:55 +0200

    Add gyro

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "quadcopter_position.h"
+#include "bno08x_drone_gyro.h"
 
-QuadcopterPosition quadcopter_position;
+static QuadcopterPosition quadcopter_position;
+static Bno08xDroneGyro gyro(10);
+static unsigned long gyro_last_run_milliseconds = 0;
+
 
 void setup() {
     Serial.begin(115200);
@@ -8,10 +12,19 @@ void setup() {
     Serial.println("START");
 
     quadcopter_position.setup();
+    gyro.setup();
+
+    gyro.printYawPitchRollAndAcceleration();
 }
 
 void loop() {
     quadcopter_position.run();
+
+    if (millis() - gyro_last_run_milliseconds > 4000)
+    {
+        gyro.reload();
+        gyro_last_run_milliseconds = millis();
+    }
 
     Serial.print("Altitude: ");
     Serial.print(quadcopter_position.getAltitude());
