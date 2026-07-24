1351a489d0a79df2974529ce961243016b61c0ea
LordMax2 <max.klevebrand@gmail.com>
2026-07-24 14:28:53 +0200

    Test scaffold first version of GPS/GNSS integration

--- include/quadcopter_position.h
+++ include/quadcopter_position.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Adafruit_BMP280.h>
+#include <HardwareSerial.h>
+#include <SparkFun_u-blox_GNSS_v3.h>
 #include "concept_drone_gyro.h"
 #include "concept_drone_position.h"
 #include "kalman_engine_3x3.h"
@@ -9,16 +11,25 @@ template <class SomeDroneGyroType>
 class QuadcopterPosition
 {
     Adafruit_BMP280 _bmp_device;
+    SFE_UBLOX_GNSS_SERIAL _gps;
+    HardwareSerial& _gps_serial;
 
     float _temperature = 0.0f;
     float _pressure = 0.0f;
     float _bmp280_last_altitude = 0.0f;
+    float _latitude = 0.0f;
+    float _longitude = 0.0f;
 
     unsigned long _last_run_timestamp_microseconds = 0;
     float _last_imu_update_seconds = 0;
 
     unsigned long _run_interval_microseconds;
     static constexpr float sea_level_pressure_pa = 101325.0f;
+    static constexpr float gps_degrees_scale = 1e-7f;
+    static constexpr unsigned long gps_baud_rate = 38400UL;
+    static constexpr uint8_t gps_navigation_frequency_hz = 10;
+
+    bool _gps_ready = false;
 
     KalmanEngine3x3 _kalman_altitude{};
 
@@ -27,7 +38,8 @@ class QuadcopterPosition
     static float pressureToAltitudeMeters(float pressure_pa, float sea_level_pressure_pa);
 
 public:
-    explicit QuadcopterPosition(SomeDroneGyroType* gyro, const int run_interval_hz = 25)
+    QuadcopterPosition(SomeDroneGyroType* gyro, HardwareSerial& gps_serial, const int run_interval_hz = 25)
+        : _gps_serial(gps_serial)
     {
         _run_interval_microseconds = 1000000UL / run_interval_hz;
         _gyro = gyro;
@@ -49,9 +61,9 @@ public:
 
     float getVelocityY() const { return 0.0f; }
 
-    float getLongitude() const { return 0.0f; }
+    float getLongitude() const { return _longitude; }
 
-    float getLatitude() const { return 0.0f; }
+    float getLatitude() const { return _latitude; }
 
     void run(bool has_gyro_update);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "bno08x_drone_gyro.h"
 
 static Bno08xDroneGyro gyro(10);
-static QuadcopterPosition<Bno08xDroneGyro> quadcopter_position(&gyro);
+static QuadcopterPosition<Bno08xDroneGyro> quadcopter_position(&gyro, Serial2);
 static unsigned long gyro_last_run_milliseconds = 0;
 
 void setup()
@@ -13,8 +13,6 @@ void setup()
 
     quadcopter_position.setup();
     gyro.setup();
-
-    gyro.printYawPitchRollAndAcceleration();
 }
 
 void loop()
@@ -34,12 +32,18 @@ void loop()
     const float altitude = quadcopter_position.getAltitude();
     const float raw_altitude = quadcopter_position.getRawAltitude();
     const float velocity_z = quadcopter_position.getVelocityZ();
+    const float latitude = quadcopter_position.getLatitude();
+    const float longitude = quadcopter_position.getLongitude();
 
     Serial.print(altitude);
     Serial.print("\t");
     Serial.print(raw_altitude);
     Serial.print("\t");
     Serial.print(velocity_z);
+    Serial.print("\t");
+    Serial.print(latitude, 7);
+    Serial.print("\t");
+    Serial.print(longitude, 7);
     Serial.println();
 
     delay(2);
